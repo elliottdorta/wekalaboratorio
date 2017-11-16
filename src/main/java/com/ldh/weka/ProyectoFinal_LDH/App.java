1880b5d6aff6908d92571db7f7b2729b07082fed
Elliott Dorta <Elliott Dorta@LAPTOP-RCB97SR4>
2017-11-16 15:47:37 +0000

    "Hola mundo!!

---
+++ src/main/java/com/ldh/weka/ProyectoFinal_LDH/App.java
@@ -0,0 +1,13 @@
+package com.ldh.weka.ProyectoFinal_LDH;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
