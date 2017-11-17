2df7da8b7f39ad6c06a4af517dc4660c32aac996
Elliott Dorta <Elliott Dorta@10.159.1.111>
2017-11-17 11:25:06 +0000

    "Arreglando avisos"

--- src/main/java/com/ldh/weka/ProyectoFinal_LDH/interfazGrafica.java
+++ src/main/java/com/ldh/weka/ProyectoFinal_LDH/interfazGrafica.java
@@ -4,10 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Color;
-import java.awt.SystemColor;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
